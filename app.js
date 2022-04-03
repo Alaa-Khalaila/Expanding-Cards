bcf34921f35f0ea126200a17deaa6b2d579790c2
Alaa-Khalaila <alkhalila.alaa@gmail.com>
2022-04-03 20:39:52 +0300

    added accessibility

--- app.js
+++ app.js
@@ -2,13 +2,23 @@ const cards = document.querySelectorAll(".card");
 
 cards.forEach((card) => {
   card.addEventListener("click", () => {
-    removeActiveClass()
+    removeActiveClass();
+    card.setAttribute("aria-expanded", "true");
     card.classList.add("active");
+  });
+
+  card.addEventListener("keyup", (event) => {
+    if (event.keyCode === 13) {
+      removeActiveClass();
+      card.setAttribute("aria-expanded", "true");
+      card.classList.add("active");
+    }
   });
 });
 
-const removeActiveClass = ()=>{
-    cards.forEach(card=>{
-        card.classList.remove("active");
-    })
-}
+const removeActiveClass = () => {
+  cards.forEach((card) => {
+    card.classList.remove("active");
+    card.setAttribute("aria-expanded", "false");
+  });
+};
